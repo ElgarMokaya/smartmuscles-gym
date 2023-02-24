61be619d0a7bc2957b4a5ccaf72a5db752b40b7f
ElgarMokaya <elgarmokaya01@gmail.com>
2023-02-24 22:14:26 +0300

    connected frontend and backend

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from  '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  API_URL:string =  'http://localhost:8000';
+  constructor(private httpClient:HttpClient) {   }
+    getMembers(){
+       return this.httpClient.get(`{this.API_URL}/members`)
+    }
 
-  constructor() { }
+
 }
+

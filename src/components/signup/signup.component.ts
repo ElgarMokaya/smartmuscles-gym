3c7e114ac35209d555fb4746269956092bd2c532
ElgarMokaya <elgarmokaya01@gmail.com>
2023-02-25 20:49:00 +0300

    created user

--- src/components/signup/signup.component.ts
+++ src/components/signup/signup.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { SignupService } from 'src/services/signup.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
+  providers:[SignupService]
 })
 export class SignupComponent {
+  
 
 }
